1d09c4e9705749a212c157dab56c9c69d9d39fcc
Antônio Neto <acm.neto1999@gmail.com>
2025-05-01 22:28:47 -0300

    :wrench: chore: ajustando a função de população de roles e adicionando um usuário administrador na migração

--- backend/database/migrations/3_add_role_id_to_users_table.php
+++ backend/database/migrations/3_add_role_id_to_users_table.php
@@ -3,13 +3,14 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use \DB;
 
 return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void {
-        $this->populateRoles();
+        $this->populate();
 
         Schema::table('users', function (Blueprint $table) {
             $table->foreignId('role_id')->default(3); // Usuário comum
@@ -30,12 +31,28 @@ return new class extends Migration {
         });
     }
 
-    private function populateRoles() {
+    /**
+     * Popula a tabela de roles e adiciona um usuário "super" administrador
+     */
+    private function populate() {
         // Verifica se os roles já existem ou insere novos
-        \DB::table('roles')->insertOrIgnore([
+        DB::table('roles')->insertOrIgnore([
             ['id' => 1, 'name' => 'Administrador'],
             ['id' => 2, 'name' => 'Operador'],
             ['id' => 3, 'name' => 'Usuário comum'],
+        ]);
+
+        // Adiciona um usuário administrador de exemplo
+        DB::table('users')->insertOrIgnore([
+            [
+                'id' => 1,
+                'name' => 'Admin User',
+                'email' => 'admin@example.com',
+                'password' => bcrypt('password'), // Certifique-se de usar uma senha segura
+                'role_id' => 1, // Administrador
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 };
