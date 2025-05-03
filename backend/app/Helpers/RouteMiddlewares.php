51d702a7eb782cc309e47298614e9383007139e2
Antônio Neto <acm.neto1999@gmail.com>
2025-05-02 21:26:10 -0300

    :wastebasket: remove: removendo middleware AdminMiddleware e sua constante correspondente

--- backend/app/Helpers/RouteMiddlewares.php
+++ backend/app/Helpers/RouteMiddlewares.php
@@ -4,6 +4,5 @@ namespace App\Helpers;
 
 class RouteMiddlewares {
     public const JWT_AUTH = \App\Http\Middleware\JwtAuthenticate::class;
-    public const ADMIN = \App\Http\Middleware\AdminMiddleware::class;
     public const ROLE = \App\Http\Middleware\RoleMiddleware::class;
 }
