637688f09e950ec2448b6ee643b21db688797f84
Antônio Neto <acm.neto1999@gmail.com>
2025-05-04 18:22:48 -0300

    :wrench: chore: adicionar suporte ao Laravel Vite Plugin no arquivo de configuração do vite

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    react(),
+  ],
   server: {
     host: true,
     port: 5173,
