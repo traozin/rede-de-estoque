dbc9adc89fe2c812f88993575c585832e9eb8287
Antônio Neto <acm.neto1999@gmail.com>
2025-05-04 23:24:20 -0300

    :sparkles: feat: atualizar rotas para usar controladores de autenticação e dashboard

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use Inertia\Inertia;
+use App\Http\Controllers\Auth\LoginController;
+use App\Helpers\RouteMiddlewares;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return Inertia::render('Login');
+Route::get('/', [LoginController::class, 'index']);
+
+Route::group(['middleware' => RouteMiddlewares::JWT_AUTH], function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 });
